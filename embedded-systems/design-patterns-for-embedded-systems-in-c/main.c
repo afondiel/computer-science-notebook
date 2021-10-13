5cabdae2291f6e5784f9a05bfb2d1e3a7fa3390b
afondiel <studenetexchange@gmail.com>
2021-10-13 08:27:19 +0200

    embedded-systems : add TEST III Sensor in object-based style

--- embedded-systems/design-patterns-for-embedded-systems-in-c/main.c
+++ embedded-systems/design-patterns-for-embedded-systems-in-c/main.c
@@ -27,11 +27,18 @@ int main()
 
 // OO or Structured : Better approach OO.
 
-/*File-based  : as CLASS */
+/*File-based  : as CLASS
+    =>file can be seen as encapsulating boundary
+    => Pair of files  : *.C (src) as funtions body and private varibles/functions
+                        *.H (header) as public variables and functions
+*/
+///////Code : CLASSICAL MODULAR PROGRAMMING : multiple *.c + *.h files ///////
 
-//file can be seen as encapsulating boundary*/
-//      => Pair of files  : *.C (src) as funtions body and private varibles/functions
-//                          *.H (header) as public variables and functions
+/*Object-based : file base + "structs" to represent the classes (instances of which comprise the
+        objects) and 'mangled functions'
+        => useful when there will multiple instances
+*/
+
     printf(" ::::::::::: TEST I Sensor ::::::::::::: \n");
     printf("Get filterFrequency : %d\n", mySensor.filterFrequency);
     printf("Get updateFrequency : %d\n", mySensor.updateFrequency);
@@ -42,10 +49,29 @@ int main()
     printf("Get updateFrequency : %d\n", Sensor_getUpdateFrequency(&mySensor));
     printf("Get value : %d\n", Sensor_getValue(&mySensor));
 
+    printf(" ::::::::::: TEST III Sensor ::::::::::::: \n");
 
-/*Object-based*/
+    Sensor * p_Sensor0, * p_Sensor1;
+    p_Sensor0 = Sensor_Create();
+    p_Sensor1 = Sensor_Create();
+    /* do stuff with the sensors ere */
+    p_Sensor0->value = 99;
+    p_Sensor1->value = -1;
+    printf("The current value from Sensor 0 is %d\n", Sensor_getValue(p_Sensor0));
+    printf("The current value from Sensor 1 is %d\n", Sensor_getValue(p_Sensor1));
+    /* done with sensors */
+    Sensor_Destroy(p_Sensor0);
+    Sensor_Destroy(p_Sensor1);
 
-/*Object-oriented*/
+/*Object-oriented : This style is similar to object-based except that the
+    => "struct" itself contains "function pointers" (virtual functions)
+    => polymorphism and inheritance
+*/
+
+// code
+
+
+
 
 //printf("Chapter 2 :  Embedded Programming with The HarmonyTM for Embedded RealTime Process\n");
 //Chapter 2  : Embedded Programming with The HarmonyTM for Embedded RealTime Process
