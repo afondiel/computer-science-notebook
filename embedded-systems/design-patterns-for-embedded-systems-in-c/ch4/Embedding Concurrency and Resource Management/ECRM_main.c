ee86edd997597db8bc75d7416eec20751542da18
afondiel <studenetexchange@gmail.com>
2021-12-16 08:31:45 +0100

    embedded-sys-design-pattern-ch4 : add ECRM_main

---
+++ embedded-systems/design-patterns-for-embedded-systems-in-c/ch4/Embedding Concurrency and Resource Management/ECRM_main.c
@@ -0,0 +1,76 @@
+
+///////////////////////////////////////////////////////
+// design-patterns-for-embedded-systems-in-c
+// by : Bruce Powel Douglass - PhD
+///////////////////////////////////////////////////////
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "ECRM_main.h"
+//#include "4.1-Basic Concurrency Concepts\include"
+//#include "4.2-Cyclic Executive Patterncepts\include"
+//#include "4.3-Static Priority Pattern\include"
+//#include "4.4-Critical Region Pattern\include"
+//#include "4.5-Guarded Call Pattern\include"
+//#include "4.6-Queuing Pattern\include"
+//#include "4.7-Rendezvous Pattern\include"
+//#include "4.8-Simultaneous Locking Pattern\include"
+//#include "4.9-Ordered Locking\include"
+
+/* ================ ECRM MAIN TEST ============================= */
+
+/**
+*@fn ECRM_main
+*@brief
+*@param
+*@return
+*/
+void ECRM_main(void)
+{
+    printf("//-------- Design Patterns for Embedding Concurrency and Resource Management-------------//\n");
+
+
+    printf("///////////////////////////\
+    //4.1-Basic Concurrency Concepts //\
+    //////////////////////////\n");
+    //?
+
+    printf("//////////////////////////\
+    //4.2-Cyclic Executive Pattern //\
+    //////////////////////////\n");
+    //?
+
+    printf("//////////////////////////\
+    //4.3-Static Priority Pattern //\
+    //////////////////////////\n");
+    //?
+
+    printf("///////////////////////////////\
+    //4.4-Critical Region Pattern//\
+    //////////////////////////////\n");
+
+     printf("///////////////////////////////\
+    //4.5-Guarded Call Pattern//\
+    //////////////////////////////\n");
+
+    printf("///////////////////////////////\
+    //4.6-Queuing Pattern	//\
+    //////////////////////////////\n");
+
+    printf("///////////////////////////////\
+    //4.7-Rendezvous Pattern	//\
+    //////////////////////////////\n");
+
+    printf("//////////////////////////////////////\
+    //4.8-Simultaneous Locking Pattern	//\
+    /////////////////////////////////////\n");
+
+    printf("///////////////////////////////\
+    //4.9-Ordered Locking	//\
+    //////////////////////////////\n");
+
+
+
+    //?
+
+}
